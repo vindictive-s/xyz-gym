f66cfd68fabe6e7ba2d11fbf0ea09bbdbda9d117
vindictive-s <se7bastian.bosoi@gmail.com>
2023-02-18 12:19:16 +0000

    xyz gym adjustted final

--- src/scenes/ContactUs/index.tsx
+++ src/scenes/ContactUs/index.tsx
@@ -28,7 +28,7 @@ const ContactUs = ({ setSelectedPage }: Props) => {
     <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
-        className=" mt-[80px]"
+        className="mt-[80px] h-[100vh]"
       >
         <motion.div
           className="md:w-3/5"
